0b330c04195446d3c87e751b5a88979183e7bfc1
Mark Duffield <duffield.mark+tech@gmail.com>
2018-04-02 18:53:39 +0100

    Adds initial implementation of CsvParser, needs replacing as does not support nesting

---
+++ src/ChannelGrabber/Converter/Parser/Input/CsvParser.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ChannelGrabber\Converter\Parser\Input;
+
+use ChannelGrabber\Converter\Parser\InputParserInterface;
+
+/**
+ * Converts a CSV string input to a multidimensional array, grouping headers with underscores into nested arrays
+ *
+ * Class CsvParser
+ * @package ChannelGrabber\Converter\Parser\Input
+ * @todo This currently only works for nesting one level deep. i.e. address_line1 but not address_lines_line1, needs replacing
+ */
+class CsvParser implements InputParserInterface
+{
+    protected CONST CSV_DELIMITER = ',';
+    /** @var array */
+    protected $structure;
+
+    /**
+     * @param mixed $data
+     * @return array $data
+     */
+    public function parse(string $data): array
+    {
+        // Turn the string of CSV data into an array, where each element contains an array of fields for each row
+        $csv = array_map('str_getcsv', str_getcsv($data,"\n"));
+
+        // Get the headings from the first row and create a formatted headings array to store the new values in
+        $headings = array_shift($csv);
+        $formattedHeadings = [];
+
+        /*
+         * Loop over the headings, find any with an underscore and create a nested array with the relevant parts,
+         * where no underscore exists, simply set the value as a value in the array
+         */
+        $count = 0;
+        foreach($headings as $heading) {
+            if (strpos($heading, '_') !== false) {
+                $outerHeading = str_split($heading, strpos($heading, '_'));
+                $formattedHeadings[$outerHeading[0]][ltrim($outerHeading[1], '_')] = $count;
+            } else {
+                $formattedHeadings[$heading] = $count;
+            }
+            $count++;
+        }
+
+        // Set the formatted headings as the structure
+        $this->structure = $formattedHeadings;
+
+        $output = $this->structure;
+
+        /**
+         * Iterate over the csv rows, then iterate over the structure.
+         *  Find the relevant row field that should be mapped to the array key (as determined above) and set it
+         */
+        foreach ($csv as $row) {
+            foreach ($this->structure as $heading => $value) {
+                if (is_array($value)) {
+                    foreach($value as $subHeading => $subValue) {
+                        $output[$heading][$subHeading] = $row[$subValue];
+                    }
+                } else {
+                    $output[$heading] = $row[$value];
+                }
+            }
+        }
+        return $output;
+    }
+}
