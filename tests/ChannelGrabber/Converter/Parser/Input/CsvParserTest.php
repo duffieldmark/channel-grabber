ba7cb03285f90b16906aa5693c44a93afb799d91
Mark Duffield <duffield.mark+tech@gmail.com>
2018-04-02 18:47:39 +0100

    Adds CsvParserTest

---
+++ tests/ChannelGrabber/Converter/Parser/Input/CsvParserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace tests\ChannelGrabber\Converter\Parser\Input;
+
+use ChannelGrabber\Converter\Parser\Input\CsvParser;
+use PHPUnit\Framework\TestCase;
+
+class ProductParserTest extends TestCase
+{
+    /** @var CsvParser */
+    protected $csvParser;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->csvParser = new CsvParser();
+    }
+
+    public function testParse()
+    {
+        $csv = "name,address_line1,address_line2\nDave,Street,Town";
+
+        $expectedOutput = $array = [
+            'name' => 'Dave',
+            'address' => [
+                'line1' => 'Street',
+                'line2' => 'Town'
+            ]
+        ];
+
+        $output = $this->csvParser->parse($csv);
+        $this->assertEquals($expectedOutput, $output);
+
+    }
+}
