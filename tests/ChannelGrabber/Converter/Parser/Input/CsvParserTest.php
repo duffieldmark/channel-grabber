79e3ae91804d30afe4c8b78a23f5323111047dfb
Mark Duffield <duffield.mark+tech@gmail.com>
2018-04-02 19:54:41 +0100

    Adds additional tests for multiline outputs

--- tests/ChannelGrabber/Converter/Parser/Input/CsvParserTest.php
+++ tests/ChannelGrabber/Converter/Parser/Input/CsvParserTest.php
@@ -16,15 +16,42 @@ class ProductParserTest extends TestCase
         $this->csvParser = new CsvParser();
     }
 
-    public function testParse()
+    public function testParseOneLine()
     {
         $csv = "name,address_line1,address_line2\nDave,Street,Town";
 
         $expectedOutput = $array = [
-            'name' => 'Dave',
-            'address' => [
-                'line1' => 'Street',
-                'line2' => 'Town'
+            [
+                'name' => 'Dave',
+                'address' => [
+                    'line1' => 'Street',
+                    'line2' => 'Town'
+                ]
+            ]
+        ];
+
+        $output = $this->csvParser->parse($csv);
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testParseTwoLine()
+    {
+        $csv = "name,address_line1,address_line2\nDave,Street,Town\nMark,Street2,Town2";
+
+        $expectedOutput = [
+            [
+                'name' => 'Dave',
+                'address' => [
+                    'line1' => 'Street',
+                    'line2' => 'Town'
+                ]
+            ],
+            [
+                'name' => 'Mark',
+                'address' => [
+                    'line1' => 'Street2',
+                    'line2' => 'Town2'
+                ]
             ]
         ];
 
--- tests/ChannelGrabber/Converter/Parser/Output/JsonParserTest.php
+++ tests/ChannelGrabber/Converter/Parser/Output/JsonParserTest.php
@@ -16,7 +16,7 @@ class ProductParserTest extends TestCase
         $this->jsonParser = new JsonParser();
     }
 
-    public function testParse()
+    public function testParseOneLine()
     {
         $data = [
             'name' => 'Dave',
@@ -27,6 +27,31 @@ class ProductParserTest extends TestCase
         ];
 
         $expectedOutput = '{"name":"Dave","address":{"line1":"Street","line2":"Town"}}';
+
+        $output = $this->jsonParser->parse($data);
+        $this->assertEquals($expectedOutput, json_encode(json_decode($output)));
+    }
+
+    public function testParseTwoLine()
+    {
+        $data = [
+            [
+                'name' => 'Dave',
+                'address' => [
+                    'line1' => 'Street',
+                    'line2' => 'Town'
+                ]
+            ],
+            [
+                'name' => 'Mark',
+                'address' => [
+                    'line1' => 'Street2',
+                    'line2' => 'Town2'
+                ]
+            ]
+        ];
+
+        $expectedOutput = '[{"name":"Dave","address":{"line1":"Street","line2":"Town"}},{"name":"Mark","address":{"line1":"Street2","line2":"Town2"}}]';
 
         $output = $this->jsonParser->parse($data);
         $this->assertEquals($expectedOutput, json_encode(json_decode($output)));
