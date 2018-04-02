7d9e346b545952e2727dde4b95d257d0bc5a5a21
Mark Duffield <duffield.mark+tech@gmail.com>
2018-04-02 19:43:35 +0100

    Removes a weird new line

--- tests/ChannelGrabber/Converter/Parser/Output/JsonParserTest.php
+++ tests/ChannelGrabber/Converter/Parser/Output/JsonParserTest.php
@@ -26,8 +26,7 @@ class ProductParserTest extends TestCase
             ]
         ];
 
-        $expectedOutput =
-            '{"name":"Dave","address":{"line1":"Street","line2":"Town"}}';
+        $expectedOutput = '{"name":"Dave","address":{"line1":"Street","line2":"Town"}}';
 
         $output = $this->jsonParser->parse($data);
         $this->assertEquals($expectedOutput, json_encode(json_decode($output)));
